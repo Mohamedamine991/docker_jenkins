e4fb24456fe383af39fa0450bebf7047a692acd8
Amine <amine.hamdi@insat.ucar.tn>
2023-11-15 15:07:58 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Hello To CI/CD Workshop</code> 
+           <code>Welcome To CI/CD Workshop</code> 
         </p>
         <a
           className="App-link"

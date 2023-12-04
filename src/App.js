8bba255fa56b9c02c0de74821c085ba02f4d890c
Amine <amine.hamdi@insat.ucar.tn>
2023-12-04 17:21:24 +0100

    lll

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>welcome to PCP consulting amine</code> 
+           <code>welcome to PCP consulting aminea</code> 
         </p>
         <a
           className="App-link"
